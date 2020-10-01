311ebf4e119229e4e4eded22c4b97d096003972e
Stanislav Orlov <stasorlov21@gmail.com>
2020-10-01 17:14:21 +0300

    Record Types

--- CSharp9Demo/1. RecordTypes.cs
+++ CSharp9Demo/1. RecordTypes.cs
@@ -8,6 +8,34 @@ namespace CSharp9Demo
 {
     internal sealed class RecordTypes
     {
+        /*
+         * Records are immutable reference types that provides synthesized methods to provide value semantics for equality.
+         */
 
+        public record Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public Person(string firstName, string lastName) => (FirstName, LastName) = (firstName, lastName);
+        }
+
+        public record Teacher : Person
+        {
+            public string Subject { get; set; }
+
+            public Teacher(string firstName, string lastName, string subject)
+                : base(firstName, lastName) => Subject = subject;
+        }
+
+        public static void Run()
+        {
+            Person person1 = new Person("Stanislav", "Orlov");
+            Person person2 = new Person("Stanislav", "Orlov");
+
+            Console.WriteLine(person1 == person2);      //false
+
+            person1.FirstName = "Vladyslav";
+        }
     }
 }
--- CSharp9Demo/Program.cs
+++ CSharp9Demo/Program.cs
@@ -2,20 +2,11 @@
 
 namespace CSharp9Demo
 {
-    public record Person
-    {
-        public string FirstName { get; }
-        public string LastName { get; }
-        public DateTime BirthDate { get; init; }
-
-        public Person(string firstName, string lastName) => (FirstName, LastName) = (firstName, lastName);
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RecordTypes.Run();
         }
     }
 }
