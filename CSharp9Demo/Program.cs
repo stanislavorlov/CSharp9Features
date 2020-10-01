2ceea29650f0660fc3d4e855572f2ce9e787bb12
Stanislav Orlov <stanislav.orlov@external.cnhind.com>
2020-10-01 16:29:09 +0300

    Initial commit

---
+++ CSharp9Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp9Demo
+{
+    public record Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public DateTime BirthDate { get; init; }
+
+        public Person(string firstName, string lastName) => (FirstName, LastName) = (firstName, lastName);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
